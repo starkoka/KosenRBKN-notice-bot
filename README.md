40c8c2e5d53125f27a55944c4bc56d01a73d1112
starkoka <j22437@inc.kisarazu.ac.jp>
2024-04-05 15:40:58 +0900

    feat:pingコマンド実装部分まで環境構築

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # KosenRBKN-notice-bot
 高専ロボコン公式HPが更新されるとお知らせするbotです
+
+## 使い方
+### 自分のサーバーで運用する場合
+index.jsと同じ階層に、以下のconfig.jsonを作成してください。
+```json
+{
+  "token": "YOUR_BOT_TOKEN",
+  "logSystem": "LOG_CHANNEL_ID",
+  "errorSystem": "ERRORLOG_CHANNEL_ID",
+  "client": "YOUR_BOT_ID"
+}
+```
+その後、deploy-commands.jsを実行してスラッシュコマンドを登録した後、index.jsを実行してください。
---
+++ commands/general.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder, EmbedBuilder , version} = require('discord.js');
+module.exports =
+[
+    {
+        data: new SlashCommandBuilder()
+            .setName('ping')
+            .setDescription('このBOTのpingを測定します'),
+        async execute(interaction) {
+            await interaction.reply( `Ping : ${interaction.client.ws.ping}ms` );
+        },
+    },
+]
