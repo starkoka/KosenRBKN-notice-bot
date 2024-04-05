4f37cfbde5cc7a5169b4db38d560484762b2a3c3
starkoka <j22437@inc.kisarazu.ac.jp>
2024-04-05 17:18:42 +0900

    feat:DBにデータを登録する部分を実装

--- functions/scraping.mjs
+++ functions/scraping.mjs
@@ -1,12 +1,25 @@
 import fetch from 'node-fetch';
 import jsdom from 'jsdom';
+import {find,updateOrInsert} from './db.js';
 
 const { JSDOM } = jsdom;
 
 export async function fetchWebsite(){
-    const url = "https://www.google.com";
+    const url = "https://official-robocon.com/kosen/";
     const res = await fetch(url);
     const html = await res.text();
-    const dom = new JSDOM(html);
-    console.log(dom.window.document.querySelector("title").textContent);
+
+    console.log(html);
+    const yesterdayData = await find("main","data",{date:0});
+    if(yesterData.length() !== 0){
+        await updateOrInsert("main","data",{date:-1},{
+            date:-1,
+            html:yesterdayData[0].html
+        });
+    }
+
+    await updateOrInsert("main","data",{date:0},{
+        date:0,
+        html:html
+    });
 }
